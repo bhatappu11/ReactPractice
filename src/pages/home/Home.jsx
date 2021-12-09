ad6ae7c80e2a9fd2256b091ccb141750b12b5471
Arpitha Bhat <arpithabht12@gmail.com>
2021-12-09 21:21:54 +0530

    [Arpitha]ADDED: link to functional lifecycle page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -16,6 +16,8 @@ function Home() {
                     <span style={{padding:'10px'}}></span>
                     <Link style={{ textDecoration: "none"}} to="/classlifecycle">CLASS_LIFECYCLE</Link>
                     <span style={{padding:'10px'}}></span>
+                    <Link style={{ textDecoration: "none"}} to="/funclifecycle">FUNCTION_LIFECYCLE</Link>
+                    <span style={{padding:'10px'}}></span>
             </div>
         </div>
     )
