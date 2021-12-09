b497b691d1ab2a851d7b70a8b599171ece22594d
Arpitha Bhat <arpithabht12@gmail.com>
2021-12-09 15:58:09 +0530

    [Arpitha]ADDED: route to props page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import State from './pages/state/State';
+import Props from './pages/props/Props';
 import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
 import Home from './pages/home/Home';
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
         <Route exact path="/state" element={<State />}></Route>
+        <Route exact path="/props" element={<Props />}></Route>
       </Routes>
     </div>
     </Router>
