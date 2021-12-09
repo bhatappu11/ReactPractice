30ca27160e4e35b0140257373f67c4382f3f8096
Arpitha Bhat <arpithabht12@gmail.com>
2021-12-09 16:10:27 +0530

    [Arpitha]ADDED: route path for functional component page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import State from './pages/state/State';
 import Props from './pages/props/Props';
 import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
 import Home from './pages/home/Home';
+import Functional from './pages/functional/Functional';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" element={<Home />}></Route>
         <Route exact path="/state" element={<State />}></Route>
         <Route exact path="/props" element={<Props />}></Route>
+        <Route exact path="/functional" element={<Functional />}></Route>
       </Routes>
     </div>
     </Router>
