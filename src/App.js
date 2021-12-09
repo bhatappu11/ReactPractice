d361b0bb55f30e2dea7e11482f917dc887fadfae
Arpitha Bhat <arpithabht12@gmail.com>
2021-12-09 15:40:20 +0530

    [Arpitha]ADDED: routing paths for home page and state page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import State from './pages/state/State';
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
+import Home from './pages/home/Home';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <State />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/state" element={<State />}></Route>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
