5c3a88ad6aacfae12e8ca94242f2194243b73777
Arpitha Bhat <arpithabht12@gmail.com>
2021-12-10 06:04:07 +0530

    [Arpitha]REFACTORED: demonstrating portal concepts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import State from './pages/state/State';
 import Props from './pages/props/Props';
@@ -9,11 +10,17 @@ import Class from './pages/class/Class';
 import ClassLifeCycle from './pages/classlifecycle/ClassLifeCycle';
 import FuncLifeCycle from './pages/functionlifecycle/FuncLifeCycle';
 import ReactHooks from './pages/hooks/ReactHooks';
+import Modal from './components/modal/Modal';
 
 function App() {
+  const showPortal = false;
+  const [showModal, setShowModal] = React.useState(false);
+  const handleShowMessageClick = () => setShowModal(true)
+  const handleCloseModal = () => setShowModal(false)
   return (
-    <Router>
     <div className="App">
+    <Router>
+    
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
         <Route exact path="/state" element={<State />}></Route>
@@ -24,8 +31,40 @@ function App() {
         <Route exact path="/funclifecycle" element={<FuncLifeCycle />}></Route>
         <Route exact path="/hooks" element={<ReactHooks />}></Route>
       </Routes>
-    </div>
+    
     </Router>
+    <div
+    style={{
+      height: '100%',
+      display: 'grid',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <div
+      style={{
+        maxWidth: 400,
+        position: 'relative',
+      }}
+    >
+      <h1>My App</h1>
+      <p>
+        This is an example of how you might use React.createPortal. I think
+        it is a pretty neat API that is yet another awesome escape hatch
+        that React provides for practical reasons. Sometimes you just need
+        to render something completely outside the React Tree.
+      </p>
+      <button onClick={handleShowMessageClick}>
+        Show Secret Modal
+      </button>
+      {showModal ? (
+        <Modal onClose={handleCloseModal}>
+          This is the secret modal message!
+        </Modal>
+      ) : null}
+    </div>
+  </div>
+  </div>
   );
 }
 
